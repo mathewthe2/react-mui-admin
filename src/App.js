8d1fa26f6569eb93b09f82c1260a8e2f9bf64db1
Mathew Chan <mchan6466@gmail.com>
2018-01-07 15:13:03 +0800

    toggle sidedrawer

--- src/App.js
+++ src/App.js
@@ -1,55 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
 
 class App extends Component {
+  constructor(props) {
+  super(props);
+
+  this.state = {
+    openSideDrawer: false,
+  }
+
+  this.onChangeSideDrawer = () => this.setState({openSideDrawer: !this.state.openSideDrawer})
+
+  }
   render() {
+    const {openSideDrawer} = this.state;
+
+    let toggleWidth = openSideDrawer ? 200 : 0;
+
+
     return (
       <div style={{   
         height: '100%',
         backgroundColor: '#eee'
         }}>
-        <div id="sidedrawer" className="mui--no-user-select">
-          <div id="sidedrawer-brand" className="mui--appbar-line-height">
-            <span className="mui--text-title">Admin</span>
-          </div>
-          <div className="mui-divider"></div>
-          <ul>
-            <li>
-              <strong>Category 1</strong>
-              <ul>
-                <li><a href="#">Item 1</a></li>
-                <li><a href="#">Item 2</a></li>
-                <li><a href="#">Item 3</a></li>
-              </ul>
-            </li>
-            <li>
-              <strong>Category 2</strong>
-              <ul>
-                <li><a href="#">Item 1</a></li>
-                <li><a href="#">Item 2</a></li>
-                <li><a href="#">Item 3</a></li>
-              </ul>
-            </li>
-            <li>
-              <strong>Category 3</strong>
-              <ul>
-                <li><a href="#">Item 1</a></li>
-                <li><a href="#">Item 2</a></li>
-                <li><a href="#">Item 3</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-        <header id="header">
+        { openSideDrawer &&
+          <Sidebar /> 
+        }
+        <header id="header" style={{left : toggleWidth}} >
           <div style={{backgroundColor: 'rgb(119, 143, 156)'}} className="mui-appbar mui--appbar-line-height">
             <div className="mui-container-fluid">
               <a className="sidedrawer-toggle mui--visible-xs-inline-block mui--visible-sm-inline-block js-show-sidedrawer">☰</a>
-              <a className="sidedrawer-toggle mui--hidden-xs mui--hidden-sm js-hide-sidedrawer">☰</a>
+              
+              
+              <a onClick={()=>this.onChangeSideDrawer()} className="sidedrawer-toggle mui--hidden-xs mui--hidden-sm js-hide-sidedrawer">☰</a>
+              
+              
+              
               <span className="mui--text-title mui--visible-xs-inline-block mui--visible-sm-inline-block">Brand.io</span>
             </div>
           </div>
         </header>
-        <div id="content-wrapper">
+        <div id="content-wrapper" style={{marginLeft : toggleWidth}} >
           <div  className="mui--appbar-height"></div>
           <div className="mui-container-fluid">
             <br/>
@@ -61,7 +53,7 @@ class App extends Component {
            <p>body</p>
            </div>
         </div>
-        <footer id="footer">
+        <footer id="footer " style={{marginLeft : toggleWidth}}>
           <div className="mui-container-fluid">
             <br/>
             Some footer here
---
+++ src/Sidebar/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class Sidebar extends Component {
+    render() {
+      return (
+        <div id="sidedrawer" className="mui--no-user-select">
+          <div id="sidedrawer-brand" className="mui--appbar-line-height">
+            <span className="mui--text-title">Admin</span>
+          </div>
+          <div className="mui-divider"></div>
+          <ul>
+            <li>
+              <strong>Category 1</strong>
+              <ul>
+                <li><a href="#">Item 1</a></li>
+                <li><a href="#">Item 2</a></li>
+                <li><a href="#">Item 3</a></li>
+              </ul>
+            </li>
+            <li>
+              <strong>Category 2</strong>
+              <ul>
+                <li><a href="#">Item 1</a></li>
+                <li><a href="#">Item 2</a></li>
+                <li><a href="#">Item 3</a></li>
+              </ul>
+            </li>
+            <li>
+              <strong>Category 3</strong>
+              <ul>
+                <li><a href="#">Item 1</a></li>
+                <li><a href="#">Item 2</a></li>
+                <li><a href="#">Item 3</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      );
+    }
+  }
+  
+  export default Sidebar;
